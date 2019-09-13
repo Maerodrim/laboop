2c07776cb01d03ea1b91445e8277fa147461fd3c
Maerodrim <stas.serg.99@mail.ru>
2019-09-13 21:58:12 +0400

    changes comfuntest

--- src/test/java/ru/ssau/tk/sergunin/lab/functions/CompositeFunctionTest.java
+++ src/test/java/ru/ssau/tk/sergunin/lab/functions/CompositeFunctionTest.java
@@ -8,9 +8,9 @@ public class CompositeFunctionTest {
 
     @Test
     public void testApply() {
-        MathFunction FuncH=new IdentityFunction();
-        MathFunction FuncF=new IdentityFunction();
+        MathFunction FuncH=new CbrtFunction();
+        MathFunction FuncF=new SqrFunction();
         MathFunction FuncG=new CompositeFunction(FuncH,FuncF);
-        assertEquals(FuncG.apply(5), 5, 0.0001);
+        assertEquals(FuncG.apply(8), 4, 0.0001);
     }
 }
