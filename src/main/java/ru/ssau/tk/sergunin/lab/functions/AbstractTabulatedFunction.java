65fd7309acbcb122f3147b3730c0c84ad45531aa
Laufinsconsca <walle.log@yandex.ru>
2019-09-13 22:24:03 +0400

    Added AbstractTabulatedFunction class

--- src/main/java/ru/ssau/tk/sergunin/lab/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/functions/AbstractTabulatedFunction.java
@@ -1,4 +1,32 @@
 package ru.ssau.tk.sergunin.lab.functions;
 
-public abstract class AbstractTabulatedFunction {
+public abstract class AbstractTabulatedFunction implements TabulatedFunction{
+
+    abstract int floorIndexOfX(double x);
+
+    abstract double extrapolateLeft(double x);
+
+    abstract double extrapolateRight(double x);
+
+    abstract double interpolate(double x, int floorIndex);
+
+    double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY)*(x - leftX)/(rightX - leftX);
+    }
+
+    @Override
+    public double apply(double x){
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        }
+        else if (x > rightBound()) {
+            return extrapolateRight(x);
+        }
+        else if (indexOfX(x) == -1){
+            return getY(indexOfX(x));
+        }
+        else {
+            return interpolate(x, floorIndexOfX(x));
+        }
+    }
 }
