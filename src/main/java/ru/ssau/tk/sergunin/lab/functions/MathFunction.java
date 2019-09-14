62b849d202b9dd0d1d71251c2f148021626949d6
Laufinsconsca <walle.log@yandex.ru>
2019-09-14 16:12:19 +0400

    Some changes in MathFunction

--- src/main/java/ru/ssau/tk/sergunin/lab/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/functions/MathFunction.java
@@ -2,6 +2,7 @@ package ru.ssau.tk.sergunin.lab.functions;
 
 public interface MathFunction {
     double apply(double x);
+
     default CompositeFunction andThen(MathFunction afterFunction){
         return new CompositeFunction(this, afterFunction);
     };
