2b74358e464bd75e5f38f60c9b1251192ade4ccf
Maerodrim <stas.serg.99@mail.ru>
2019-09-29 09:10:03 +0400

    диффиринциальный оператор начало

--- src/main/java/ru/ssau/tk/sergunin/lab/operations/DifferentialOperator.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/operations/DifferentialOperator.java
@@ -1,7 +1,8 @@
 package ru.ssau.tk.sergunin.lab.operations;
 
 import ru.ssau.tk.sergunin.lab.functions.MathFunction;
+import ru.ssau.tk.sergunin.lab.functions.TabulatedFunction;
 
-public interface DifferentialOperator extends MathFunction {
-    <T> T derive(T function);
+public interface DifferentialOperator<T extends MathFunction> extends MathFunction {
+   public  T derive(T function);
 }
--- src/main/java/ru/ssau/tk/sergunin/lab/operations/TabulatedDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/operations/TabulatedDifferentialOperator.java
@@ -1,10 +1,11 @@
 package ru.ssau.tk.sergunin.lab.operations;
 
+import ru.ssau.tk.sergunin.lab.functions.MathFunction;
 import ru.ssau.tk.sergunin.lab.functions.TabulatedFunction;
 import ru.ssau.tk.sergunin.lab.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.sergunin.lab.functions.factory.TabulatedFunctionFactory;
 
-public class TabulatedDifferentialOperator implements DifferentialOperator {
+public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction> {
     TabulatedFunctionFactory factory;
 
     public TabulatedDifferentialOperator() {
@@ -20,16 +21,16 @@ public class TabulatedDifferentialOperator implements DifferentialOperator {
     }
 
     @Override
-    public <TabulatedFunction> TabulatedFunction derive(TabulatedFunction function) {
-        double[] xValues = new double[TabulatedFunctionOperationService.getCount];
-        double[] yValues = new double[TabulatedFunctionOperationService.getCount];
-        TabulatedFunctionOperationService.asPoints(factory);
-        return null;
+    public double apply(double x) {
+        return 0;
     }
 
 
     @Override
-    public double apply(double x) {
-        return 0;
+    public TabulatedFunction derive(TabulatedFunction function) {
+        double[] xValues = new double[function.getCount()];
+        double[] yValues = new double[function.getCount()];
+        TabulatedFunctionOperationService.asPoints(factory);
+        return null;
     }
 }
