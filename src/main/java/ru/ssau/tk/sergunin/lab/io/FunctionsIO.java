907097c45a6137ab8b096344d0cacbe846df0619
Laufinsconsca <walle.log@yandex.ru>
2019-10-06 20:32:08 +0400

    Added writeTabulatedFunction() implemented via character stream

--- src/main/java/ru/ssau/tk/sergunin/lab/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/io/FunctionsIO.java
@@ -1,7 +1,28 @@
 package ru.ssau.tk.sergunin.lab.io;
 
+import ru.ssau.tk.sergunin.lab.functions.Point;
+import ru.ssau.tk.sergunin.lab.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point: function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        try {
+            writer.flush();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
