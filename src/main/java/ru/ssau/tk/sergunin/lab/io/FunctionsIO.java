52d9a2a83d6a16c20676d42d2caeb1ec3829e0c5
Laufinsconsca <walle.log@yandex.ru>
2019-10-11 17:18:49 +0400

    Added readTabulatedFunction() implemented via character stream

--- src/main/java/ru/ssau/tk/sergunin/lab/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/io/FunctionsIO.java
@@ -2,10 +2,15 @@ package ru.ssau.tk.sergunin.lab.io;
 
 import ru.ssau.tk.sergunin.lab.functions.Point;
 import ru.ssau.tk.sergunin.lab.functions.TabulatedFunction;
+import ru.ssau.tk.sergunin.lab.functions.factory.TabulatedFunctionFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -23,5 +28,28 @@ public final class FunctionsIO {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count;
+        try {
+            count = Integer.parseInt(reader.readLine());
+        } catch (NumberFormatException nfe) {
+            throw new IOException(nfe);
+        }
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        String tempString;
+        for (int i = 0; i < count; i++) {
+            tempString = reader.readLine();
+            try {
+                xValues[i] = formatter.parse(tempString.split(" ")[0]).doubleValue();
+                yValues[i] = formatter.parse(tempString.split(" ")[1]).doubleValue();
+            } catch (ParseException pe) {
+                throw new IOException(pe);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
