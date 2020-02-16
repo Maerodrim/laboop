7ccb821dcc334efcf2309cb5dbe80a4dfc528e02
Maerodrim <43945193+Maerodrim@users.noreply.github.com>
2020-02-16 17:59:23 +0400

    рабочий коммит метода ньютона

--- src/main/java/ru/ssau/tk/sergunin/lab/numericalMethods/NewtonMethod.java
+++ src/main/java/ru/ssau/tk/sergunin/lab/numericalMethods/NewtonMethod.java
@@ -4,15 +4,16 @@ import ru.ssau.tk.sergunin.lab.functions.MathFunction;
 import ru.ssau.tk.sergunin.lab.functions.PolynomialFunction;
 import ru.ssau.tk.sergunin.lab.operations.MiddleSteppingDifferentialOperator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class NewtonMethod implements NumericalMethod {
     public List<Double> solve(PolynomialFunction func, Double left, Double right, Integer count, Double eps) {
         double x1 = 0, x0 = 0,x=left,step;
-        List<Double> X = null;
+        List<Double> X = new ArrayList<>();
         step=(right-left)/count;
-        while(x!=right) {
+        while((x+step)<right) {
             if (func.apply(x)*func.apply(x+step)<=0) {
                 x0=x;
                 x1 = x0 - func.apply(x0) / (new MiddleSteppingDifferentialOperator(eps).derive((MathFunction) func)).apply(x0); // первое приближение
